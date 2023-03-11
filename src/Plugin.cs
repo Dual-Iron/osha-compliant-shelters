a9b82266938286b9c88e9f86102ba0488cfb8249
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-03-10 19:57:35 -0500

    Fix jolly co-op and forbidden shelters

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using RWCustom;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Permissions;
@@ -29,6 +30,10 @@ sealed class Plugin : BaseUnityPlugin
             : Mathf.CeilToInt(Options.SleepTime.Value * 40));
     }
 
+    float MinSleepPercent(RainWorldGame game)
+    {
+        return game.PlayersToProgressOrWin.Min(c => c.realizedCreature is Player p ? SleepPercent(p) : 0);
+    }
     float SleepPercent(Player self)
     {
         return Mathf.Clamp01(1f * (Data(self).sleepTime - startSleep) / (MaxSleepTime(self) - startSleep));
@@ -44,6 +49,10 @@ sealed class Plugin : BaseUnityPlugin
 
         On.HUD.FoodMeter.GameUpdate += FoodMeter_GameUpdate; // Fix line sprite jittering when it shouldn't
         On.HUD.FoodMeter.MeterCircle.Update += FoodMeter_Update; // Fix HUD flashing red when not trying to sleep
+
+        On.ShelterDoor.Update += EjectStuck;
+        On.ShortcutHandler.SpitOutCreature += ForbidEntry;
+        On.ShortcutHandler.VesselAllowedInRoom += ForbidEntryAgain;
     }
 
     private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
@@ -66,6 +75,7 @@ sealed class Plugin : BaseUnityPlugin
         if (self.Stunned || self.room?.game.session is not StoryGameSession sess || !self.room.abstractRoom.shelter || self.room.shelterDoor == null
             || self.room.shelterDoor.Broken
             || self.room.shelterDoor.closedFac != 0 && self.room.shelterDoor.closeSpeed < 0 // shelter still opening
+            || !ShelterDoor.CoordInsideShelterRange(self.abstractCreature.pos.Tile, self.room.shelterDoor.isAncient)
             || self.FoodInRoom(self.room, eatAndDestroy: false) < 1
             || self.FoodInRoom(self.room, eatAndDestroy: false) < sess.characterStats.maxFood && sess.characterStats.malnourished
             ) {
@@ -88,7 +98,7 @@ sealed class Plugin : BaseUnityPlugin
             sleepTime = 0;
         }
 
-        // Close doors when ready
+        // Close doors when ready (if-check is just an optimization)
         if (sleepTime >= MaxSleepTime(self)) {
             self.room.shelterDoor.Close();
         }
@@ -102,7 +112,7 @@ sealed class Plugin : BaseUnityPlugin
         }
         int time = Data(self).sleepTime;
         if (self.forceSleepCounter > 0 && time > 0)
-            self.forceSleepCounter = Mathf.CeilToInt(259 * SleepPercent(self)); // 260 softlocks the player, so leave it at 259
+            self.forceSleepCounter = Mathf.CeilToInt(259 * MinSleepPercent(self.room.game)); // 260 softlocks the player, so leave it at 259
         else
             self.forceSleepCounter = 0;
     }
@@ -114,26 +124,36 @@ sealed class Plugin : BaseUnityPlugin
             return;
         }
 
-        var relevantPlayers = self.room.game.PlayersToProgressOrWin.Select(p => p.realizedObject).Where(p => p != null && p.room == self.room).OfType<Player>();
-        if (!Options.SleepTogether.Value) {
-            relevantPlayers = relevantPlayers.Take(1);
-        }
-        if (!relevantPlayers.Any()) {
-            orig(self);
+        if (self.room.game.PlayersToProgressOrWin.Any(p => !ShelterDoor.CoordInsideShelterRange(p.pos.Tile, self.isAncient))) {
             return;
         }
+
+        var relevantPlayers = self.room.game.PlayersToProgressOrWin.Select(p => p.realizedObject).OfType<Player>().Where(p => p != null && p.room == self.room && !p.dead);
+
+        bool any = false;
         foreach (Player plr in relevantPlayers) {
+            plr.readyForWin = true;
+            plr.ReadyForWinJolly = true;
+
             int sleepTime = Data(plr).sleepTime;
             if (sleepTime < MaxSleepTime(plr)) {
-                return;
+                if (Options.SleepTogether.Value) return;
+                else continue;
             }
+
+            any = true;
+        }
+        if (!any) {
+            return;
         }
 
         orig(self);
 
-        // Make sleeping players stay sleeping
-        if (self.closeSpeed > 0) {
-            foreach (Player plr in relevantPlayers) {
+        foreach (Player plr in relevantPlayers) {
+            plr.readyForWin = false;
+            plr.ReadyForWinJolly = false;
+
+            if (self.closeSpeed > 0) {
                 plr.sleepCounter = -24;
             }
         }
@@ -157,5 +177,47 @@ sealed class Plugin : BaseUnityPlugin
         if (self.meter.hud.owner is Player p2) {
             p2.stillInStartShelter = false;
         }
+    }
+
+    private void EjectStuck(On.ShelterDoor.orig_Update orig, ShelterDoor self, bool eu)
+    {
+        orig(self, eu);
+
+        if (self.isAncient || self.Closed <= 0 || self.room.shortcuts == null) {
+            return;
+        }
+
+        // When the door closes...
+        IntVector2 shortcutPos = self.room.LocalCoordinateOfNode(0).Tile;
+        Vector2 depositPos = self.room.MiddleOfTile(self.room.LocalCoordinateOfNode(0)) + self.dir * 110;
+
+        foreach (PhysicalObject obj in self.room.physicalObjects.SelectMany(p => p).ToList()) {
+            // Shove creatures still in the entrance back through
+            if (obj is Creature crit && crit.bodyChunks.Any(c => self.room.GetTilePosition(c.pos) == shortcutPos)) {
+                crit.SuckedIntoShortCut(shortcutPos, false);
+                continue;
+            }
+            // Shove everything else INTO the shelter
+            foreach (var c in obj.bodyChunks) {
+                if (self.room.GetTile(c.pos).Solid) {
+                    c.pos = depositPos;
+                    c.vel = self.dir * 5;
+                }
+            }
+        }
+    }
+
+    private void ForbidEntry(On.ShortcutHandler.orig_SpitOutCreature orig, ShortcutHandler self, ShortcutHandler.ShortCutVessel vessel)
+    {
+        orig(self, vessel);
+
+        if (vessel.room.realizedRoom.shelterDoor != null && vessel.room.realizedRoom.shelterDoor.Closed > 0) {
+            self.SuckInCreature(vessel.creature, vessel.creature.room, vessel.creature.room.shortcutData(vessel.pos));
+        }
+    }
+
+    private bool ForbidEntryAgain(On.ShortcutHandler.orig_VesselAllowedInRoom orig, ShortcutHandler self, ShortcutHandler.Vessel vessel)
+    {
+        return orig(self, vessel) && !(vessel.room.realizedRoom?.shelterDoor != null && vessel.room.realizedRoom.shelterDoor.Closed > 0);
     }
 }
